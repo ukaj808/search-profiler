59c374ef57e1953b25a9aecefc5eb231d4bc10db
ukaj808 <ukaj808@gmail.com>
2022-03-30 16:06:59 -0400

    Adding skeletons for detailed cocktai view and filter functionality

--- src/search/search.api.ts
+++ src/search/search.api.ts
@@ -1,27 +1,24 @@
-import {Drink} from "./cocktaildb/model/drink.model";
-import {Ingredient} from "./cocktaildb/model/ingredient.model";
+import { Drink } from './cocktaildb/model/drink.model';
+import { Ingredient } from './cocktaildb/model/ingredient.model';
 
 export interface CocktailResult {
-    drinks?: Drink[];
-    ingredients?: Ingredient[];
+  drinks?: Drink[];
+  ingredients?: Ingredient[];
 }
 
 export interface SearchResults {
-
-    profileId: string;
-    searchItems?: SearchItem[];
-
+  profileId: string;
+  searchItems?: SearchItem[];
 }
 
 export interface SearchItem {
-    category: string;
-    items: any[];
+  category: string;
+  items: any[];
 }
 
-
 export interface SearchRequest {
-    searchStr: string;
-    type: string;
-    category?: string;
-    profileId?: string;
+  searchStr: string;
+  type: string;
+  category?: string;
+  profileId?: string;
 }
--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SearchService } from './search.service';
-import {SearchRequest, SearchResults} from "./search.api";
+import { SearchRequest, SearchResults } from './search.api';
 
 @Controller('search')
 export class SearchController {
@@ -10,5 +10,4 @@ export class SearchController {
   search(@Body() request: SearchRequest): Promise<SearchResults> {
     return this.searchService.search(request);
   }
-
 }
